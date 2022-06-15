14bef8cdd3ffe480daa65d4d11ac0392354eeba4
lavanya <lavanya.rajalingam@gmail.com>
2022-06-15 09:21:21 +0000

    updated files

--- tests.py
+++ tests.py
@@ -1,21 +1,18 @@
 import unittest
 
-from translator import english_to_french
+from translator import english_to_french, english_to_german
 
 class TestEnglish_To_French(unittest.TestCase): 
     def test1(self): 
-        self.assertEqual(english_to_french(null), null) # test input is null
-        self.assertEqual(english_to_french('hello'), 'bonjour')  # test hello translates to bonjour.
-        self.assertNotEqual(english_to_french('good'), 'mal')  # test when good doesnot translate to mal.
+        self.assertEqual(english_to_french(""), "null") # test input is null
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')  # test hello translates to bonjour.
+        self.assertNotEqual(english_to_french('Good'), 'Mal')  # test when good doesnot translate to mal.
 
-    unittest.main()
-
-from translator import english_to_german
-
+    
 class TestEnglish_To_German(unittest.TestCase): 
     def test1(self): 
-        self.assertEqual(english_to_german(null), null) # test input is null
-        self.assertEqual(english_to_german('hello'), 'hallo')  # test hello translates to hallo.
-        self.assertEqual(english_to_german('honey'), 'honig')  # test when honey translates to honig.
+        self.assertEqual(english_to_german(""), "null") # test input is null
+        self.assertEqual(english_to_german('Hello'), 'Hallo')  # test hello translates to hallo.
+        self.assertEqual(english_to_german('Honey'), 'Honig')  # test when honey translates to honig.
 
-    unittest.main()
+unittest.main()
--- translator.py
+++ translator.py
@@ -1,21 +1,53 @@
-def english_to_french()
-
 from ibm_watson import LanguageTranslatorV3
-url_lt='https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/be85874b-7169-46e2-810e-ae031d4176cb'
-apikey_lt='NaUSQjavA7-xdWkAyuoYQbQHjq4fmwXjz2mTKCV22g5q'
-version_lt='2018-05-01'
-authenticator = IAMAuthenticator(apikey_lt)
-language_translator = LanguageTranslatorV3(version=version_lt,authenticator=authenticator)
-language_translator.set_service_url(url_lt)
-language_translator
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-from pandas import json_normalize
-json_normalize(language_translator.list_identifiable_languages().get_result(), "languages")
+def english_to_french(text):
+    """
+    This function translates english to french
+    """
+    if text=="":
+        return "null"
+    
+    url_lt='https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/be85874b-7169-46e2-810e-ae031d4176cb'
+    apikey_lt='NaUSQjavA7-xdWkAyuoYQbQHjq4fmwXjz2mTKCV22g5q'
+    version_lt='2018-05-01'
+    authenticator = IAMAuthenticator(apikey_lt)
+    language_translator = LanguageTranslatorV3(version=version_lt,authenticator=authenticator)
+    language_translator.set_service_url(url_lt)
+    language_translator
+    
+    #import pandas
+    #from pandas import json_normalize
+    #json_normalize(language_translator.list_identifiable_languages().get_result(), "languages")
+    translation_response = language_translator.translate(text=text, model_id='en-fr').get_result()
 
-French_translation=language_translator.translate(text=translation_eng , model_id='en-fr').get_result()
-French_translation['translations'][0]['translation']
+    #French_translation=language_translator.translate(text=translation_eng , model_id='en-fr').get_result()
+    French_translation=translation_response['translations'][0]['translation']
+    #French_translation['translations'][0]['translation']
+    return French_translation
 
-def english_to_german()
+def english_to_german(text):
+    
+    """
+    This function translates english to german
+    """
 
-German_translation=language_translator.translate(text=translation_eng , model_id='en-de').get_result()
-German_translation['translations'][0]['translation']
+    if text=="":
+        return "null"
+    
+    url_lt='https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/be85874b-7169-46e2-810e-ae031d4176cb'
+    apikey_lt='NaUSQjavA7-xdWkAyuoYQbQHjq4fmwXjz2mTKCV22g5q'
+    version_lt='2018-05-01'
+    authenticator = IAMAuthenticator(apikey_lt)
+    language_translator = LanguageTranslatorV3(version=version_lt,authenticator=authenticator)
+    language_translator.set_service_url(url_lt)
+    language_translator
+
+#     #import pandas 
+#     #from pandas import json_normalize
+#     #json_normalize(language_translator.list_identifiable_languages().get_result(), "languages")
+    
+    translation_response = language_translator.translate(text=text, model_id='en-de').get_result()
+
+    German_translation=translation_response['translations'][0]['translation']
+    return German_translation
